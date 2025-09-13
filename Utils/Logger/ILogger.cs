36ae4c2c6beb961044b2ecf865ae00605319ab88
Nhut Phat <vongthien1232004@gmail.com>
2025-09-13 20:23:41 +0700

    add logger and setup config

---
+++ Utils/Logger/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CuahangNongduoc.Utils.Logger
+{
+    public interface ILogger
+    {
+        void Info(string message);
+        void Error(string message, Exception ex);
+        void Debug(string message);
+
+    }
+}
---
+++ Utils/Logger/Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace CuahangNongduoc.Utils.Logger
+{
+    public class Logger<T> : ILogger
+    {
+        private readonly string _className;
+        private readonly Level _current = Level.Debug;
+        public static readonly string ConfigKey = "LogLevel";
+        public Logger()
+        {
+            _className = typeof(T).Name;
+            string configLevel = ConfigurationManager.AppSettings[ConfigKey];
+            try
+            {
+                Level level = (Level)Enum.Parse(typeof(Level), configLevel, true);
+                _current = level;
+            }
+            catch (Exception ex)
+            {
+                this.Error("Missing log config", ex);
+            }
+        }
+
+        private void Log(Level level, string message, Exception ex = null)
+        {
+            if (level < _current) return;
+
+            string logMessage = $"[{DateTime.Now}][{_className}][{level}] {message}";
+            Console.WriteLine(logMessage);
+            if (ex != null)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            this.WriteLog(logMessage, ex);
+
+        }
+
+        private void WriteLog(string message, Exception ex)
+        {
+            // Write log to file or database
+            System.IO.File.AppendAllText("app.log", message + Environment.NewLine);
+            if (ex != null)
+            {
+                System.IO.File.AppendAllText("app.log", ex.ToString() + Environment.NewLine);
+            }
+        }
+
+        public void Info(string message) => this.Log(Level.Info, message);
+        public void Error(string message, Exception ex) => this.Log(Level.Error, message, ex);
+        public void Debug(string message) => this.Log(Level.Debug, message);
+    }
+}
