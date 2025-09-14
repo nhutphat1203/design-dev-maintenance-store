ff316f4419191b2763c906e2b5661a8b55667753
Nhut Phat <vongthien1232004@gmail.com>
2025-09-14 17:24:41 +0700

    update DonViTinhFactory

--- DataLayer/DonViTinhFactory.cs
+++ DataLayer/DonViTinhFactory.cs
@@ -3,32 +3,43 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data;
 using System.Data.OleDb;
+using CuahangNongduoc.DataAccess;
+using System.Data.SqlClient;
+using CuahangNongduoc.Utils.Logger;
 
 namespace CuahangNongduoc.DataLayer
 {
     public class DonViTinhFactory
     {
-        DataService m_Ds = new DataService();
+
+        private readonly DataAccessObj da = new DataAccessObj();
+        private static readonly ILogger logger = new Logger<DonViTinhFactory>();
+        public DonViTinhFactory()
+        {
+            logger.Debug("Initialized DonViTinhFactory");
+        }
 
         public DataTable DanhsachDVT()
         {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM DON_VI_TINH");
-            m_Ds.Load(cmd);
 
-            return m_Ds;
+            SqlCommand cmd = new SqlCommand("SELECT * FROM DON_VI_TINH");
+
+            da.Execute(cmd);
+
+            return da;
         }
 
 
         public DataTable LayDVT(int id)
         {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM DON_VI_TINH WHERE ID = @id");
-            cmd.Parameters.Add("id", OleDbType.Integer).Value = id;
-            m_Ds.Load(cmd);
-            return m_Ds;
+            SqlCommand cmd = new SqlCommand("SELECT * FROM DON_VI_TINH WHERE ID = @id");
+            cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+            da.Execute(cmd);
+            return da;
         }
         public bool Save()
         {
-            return m_Ds.ExecuteNoneQuery() > 0;
+            return da.ExecuteNoneQuery() > 0;
         }
     }
 }
