be80319e9d53e8cb2d09a5782e00773e8143461b
Nhut Phat <vongthien1232004@gmail.com>
2025-10-07 09:48:10 +0700

    fix rp

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace CuahangNongduoc
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
+
         }
     }
 }
